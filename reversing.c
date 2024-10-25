76ad823b00f8614ee1fa9a82c3a729601368b8c6
mamun-prodhan <mamunpr9@gmail.com>
2024-10-25 06:55:05 +0600

    reversing implemented

--- reversing.c
+++ reversing.c
@@ -2,6 +2,24 @@
 
 int main(){
 
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(int i = 0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = n-1, j = 0; i>=j; i--, j++){
+        int temp = a[j];
+        a[j] = a[i];
+        a[i] = temp;
+    }
+
+    for(int i = 0; i<n; i++){
+        printf("%d ", a[i]);
+    }
 
 
 
